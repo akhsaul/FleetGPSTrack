6de973aa78e383f829a32b302eeae4ebc0480f6a
Akhsaul <ikhsanmaulana384@gmail.com>
2025-05-03 12:01:20 +0700

    Initial project springboot with jpa postgresql

---
+++ src/main/kotlin/io/github/akhsaul/fleetgpstrack/FleetGpsTrackApplication.kt
@@ -0,0 +1,11 @@
+package io.github.akhsaul.fleetgpstrack
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FleetGpsTrackApplication
+
+fun main(args: Array<String>) {
+    runApplication<FleetGpsTrackApplication>(*args)
+}
